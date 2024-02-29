f982998d217be3ec7eac75b17851592b9594b56f
ElisaBorba <elisaborbaa@gmail.com>
2024-02-28 21:52:09 -0300

    Qalquer coisa

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -8,49 +8,54 @@ class MenuData:
     def __init__(self, source_path: str) -> None:
         self.source_path = source_path
         self.dishes = {}
+        self.menu_data = self.read_menu_data()
 
-    def __repr__(self) -> str:
-        return f"Menu('{self.dishes}')"
+    # def __repr__(self) -> str:
+    #     return f"Menu('{self.dishes}')"
 
-    def __eq__(self, other) -> bool:
-        return self.__repr__() == other.__repr__()
+    # def __eq__(self, other) -> bool:
+    #     return self.__repr__() == other.__repr__()
 
-    def __hash__(self) -> int:
-        return hash(self.__repr__())
+    # def __hash__(self) -> int:
+    #     return hash(self.__repr__())
 
     def read_menu_data(self):
-        with open(self.source_path, "r", encoding="utf-8", newline="") as file:
+        with open(self.source_path, "r") as file:
             menu_reader = csv.DictReader(file)
 
             for row in menu_reader:
-                dish = row["dish"]
-                price = row["price"]
-                ingredient = row["ingredient"]
-                recipe_amount = row["recipe_amount"]
-
-                ingredient_instance = Ingredient(ingredient)
-                dish_instance = Dish(dish, float(price))
-
-                if dish_instance.name not in self.dishes:
-                    self.dishes[dish_instance.name] = {
-                        "price": price,
-                        "ingredients": {},
-                    }
-
-                self.dishes[dish_instance.name]["ingredients"][
-                    ingredient_instance.name
-                ] = recipe_amount
+                dish = Dish(row["dish"], float(row["price"]))
+                print(dish.name)
+                if dish.name not in self.dishes:
+                    dish.add_ingredient_dependency(
+                        Ingredient(row["ingredient"]),
+                        int(row["recipe_amount"]),
+                    )
+                    # print(dish.get_ingredients())
+                    self.dishes[dish.name] = dish
+                else:
+                    # print(self.dishes)
+                    self.dishes[dish.name].add_ingredient_dependency(
+                        Ingredient(row["ingredient"]),
+                        int(row["recipe_amount"]),
+                    )
+                    # print(self.dishes[dish.name].get_ingredients())
         return set(self.dishes)
 
-    # def get_menu(self):
-    #     return set(self.dishes.keys())
+        #     self.dishes[dish_instance.name] = {
+        #         "price": price,
+        #         "ingredients": {},
+        #     }
+
+        # self.dishes[dish_instance.name]["ingredients"][
+        #     ingredient_instance.name
+        # ] = recipe_amount
 
 
 # source_path
 path = os.path.join("data", "menu_base_data.csv")
 menu_obj = MenuData(path)
 test = menu_obj.read_menu_data()
-# test_get = menu_obj.get_menu()
 print("MENUUUU", test)
 
 dishesss = menu_obj.dishes
